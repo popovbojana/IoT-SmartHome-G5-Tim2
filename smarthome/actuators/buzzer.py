0ca3c97adc887eb6cdd0098df7fba66f99b25cae
VuksanFilip <filipvuksan.iphone@gmail.com>
2024-01-12 17:36:20 +0100

    commit

--- smarthome/actuators/buzzer.py
+++ smarthome/actuators/buzzer.py
@@ -37,17 +37,3 @@ def run_buzzer_loop(buzzer, callback, stop_event, settings, publish_event, alarm
 
         if stop_event.is_set():
             break
-
-
-    while not stop_event.is_set():
-        start_time = time.time()  # Record the start time before waiting for the event
-        alarm_event.wait()
-        end_time = time.time()  # Record the end time when the event is set
-        alarm_duration = end_time - start_time
-        print("ALARM DURATIONNNNN", str(alarm_duration))
-        callback(alarm_duration, "CODE", settings, publish_event)
-
-
-    #     if stop_event.is_set():
-    #         break
-    #     time.sleep(int(duration))
