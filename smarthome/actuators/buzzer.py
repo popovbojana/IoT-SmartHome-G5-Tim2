39d7da79b57327bdb4aee0bcdac0fd69a8af5305
VuksanFilip <filipvuksan.iphone@gmail.com>
2024-01-12 17:26:45 +0100

    commit

--- smarthome/actuators/buzzer.py
+++ smarthome/actuators/buzzer.py
@@ -7,21 +7,38 @@ class BUZZER:
     def __init__(self, name, pin):
         self.name = name
         self.pin = pin
+        self.pitch = 440
         GPIO.setup(self.pin, GPIO.OUT)
 
-    def buzz(self, pitch, duration):
-        period = 1.0 / pitch
+    # def buzz(self, pitch, duration)
+    def buzz(self):
+        period = 1.0 / self.pitch
         delay = period / 2
-        cycles = int(duration * pitch)
-
-        for i in range(cycles):
-            GPIO.output(self.pin, True)
-            time.sleep(delay)
-            GPIO.output(self.pin, False)
-            time.sleep(delay)
+        GPIO.output(self.pin, True)
+        time.sleep(delay)
+        GPIO.output(self.pin, False)
+        time.sleep(delay)
 
 
-def run_buzzer_loop(buzzer, callback, stop_event, settings, publish_event, alarm_event):
+def run_buzzer_loop(buzzer, callback, stop_event, settings, publish_event, alarm_event, system_event):
+    while True:
+        alarm_event.wait()
+        start = time.time()
+        buzzer.buzz()
+
+        while system_event.is_set():
+            buzzer.buzz()
+
+        end = time.time()
+        duration = end - start
+        print("Duration:", str(duration))
+        callback(duration, "CODE", settings, publish_event)
+        alarm_event.clear()
+
+        if stop_event.is_set():
+            break
+
+
     while not stop_event.is_set():
         start_time = time.time()  # Record the start time before waiting for the event
         alarm_event.wait()
