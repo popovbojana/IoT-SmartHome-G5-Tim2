cef80f52ff3314e8edc414bd192b101d84346584
popovbojana <popov.bojana@gmail.com>
2024-01-24 00:25:25 +0100

    small changes

--- front/smarthome/src/app/pi1/pi1.component.ts
+++ front/smarthome/src/app/pi1/pi1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pi1',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./pi1.component.css']
 })
 export class Pi1Component {
+  grafanaUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.grafanaUrl = this.sanitizer.bypassSecurityTrustResourceUrl('http://localhost:3000/d/f49a4897-ab0b-4f5a-aee3-b60db1f61fe5/dht?orgId=1&from=1706028288518&to=1706049888518&viewPanel=1');
+  }
 }
